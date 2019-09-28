b3e7c647b700ff5666bffd39b567d517af012f86
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-09-28 13:25:57 +0530

    Update

--- GUI/linkers/main.js
+++ GUI/linkers/main.js
@@ -4,12 +4,11 @@ function addition() {
 var options = {
 		mode: 'text',
     	scriptPath : '../Engine/',
-    	pythonPath : 'C:/Users/DELL/AppData/Local/Programs/Python/Python37-32'
 	}
 
   ps.PythonShell.run('addition.py', options, function (err, results) {
     if (err) throw err;
-    swal(results[0]);
+    console.log(results[0]);
   });
 
 } // end of the function definition addition
