3066684c7ae5253068301d6edf510c37579596f0
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-09-28 20:37:21 +0530

    Update

--- GUI/linkers/main.js
+++ GUI/linkers/main.js
@@ -12,3 +12,30 @@ var options = {
   });
 
 } // end of the function definition addition
+
+function multiplication() {
+var options = {
+		mode: 'text',
+    	scriptPath : '../../Engine/',
+	}
+
+  ps.PythonShell.run('multiplication.py', options, function (err, results) {
+    if (err) throw err;
+    console.log(results[0]);
+  });
+
+} // end of the function definition addition
+
+
+function gen_fake_data() {
+var options = {
+		mode: 'text',
+    	scriptPath : '../../Engine/',
+	}
+
+  ps.PythonShell.run('gen_fake_data.py', options, function (err, results) {
+    if (err) throw err;
+    console.log(results);
+  });
+
+} // end of the function definition addition
